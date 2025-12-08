e92d69b690ef6b7f0926d5136bce47237542b45e
Fernando_Montenegro <158318208+Fernandinho2024@users.noreply.github.com>
2025-12-08 09:48:21 -0300

    Add files via upload

---
+++ problema1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int n1;
+    int n2;
+    int n3;
+    int media;
+    printf("Digite a nota 1:\n");
+    scanf("%d", &n1);
+    printf("Digite a nota 2:\n");
+    scanf("%d", &n2);
+    printf("Digite a nota 3:\n");
+    scanf("%d", &n3);
+    media = (n1+n2+n3) / 3;
+    if (media >= 6){
+        printf("APROVADO! :)\n");
+    }
+    else {
+        printf("REPROVADO :(\n");
+    }
+    printf("SUA MEDIA FOI %d\n",media);
+    return 0;
+}
