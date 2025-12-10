5fb57423a67646df18c7774d35b528fac632752c
ArthurDutraRecco <artreccogd762@gmail.com>
2025-12-10 10:16:53 -0300

    Update problema2.c

--- problema2.c
+++ problema2.c
@@ -4,7 +4,7 @@ int main(){
     int n;
     int divisor;
     int primo = 1;
-    printf("Digite um numero maior que 2:\n");
+    printf("Digite um numero maior que 2: ");
     scanf("%d", &n);
     for (int divisor = 2; divisor <= n/2; divisor++){
         if (n % divisor == 0 && primo){
@@ -20,5 +20,6 @@ int main(){
     }
     return 0;
 }
+
 
 
